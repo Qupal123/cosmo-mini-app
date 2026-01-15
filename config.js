310fad855da5274995c8030e6f807cab3ae933df
Qupal123 <grt_qupal@mail.ru>
2026-01-15 20:33:41 +0600

    Add files via upload

--- config.js
+++ config.js
@@ -4,21 +4,20 @@ const ADMIN_ID = 8382113817;
 const PAYMENT_PHONE = "+7 908 314 22 69";
 
 const PRODUCTS = [
- {id:1,name:"Гемы 30",price:255,img:"img/30.png"},
- {id:2,name:"Гемы 80",price:600,img:"img/80.png"},
- {id:3,name:"Гемы 170",price:1100,img:"img/170.png"},
- {id:4,name:"Гемы 360",price:2100,img:"img/360.png"},
- {id:5,name:"Гемы 950",price:5150,img:"img/950.png"},
- {id:6,name:"Гемы 2000",price:10350,img:"img/2000.png"},
- {id:7,name:"Brawl Pass",price:950,img:"img/BrawlPass.png"},
- {id:8,name:"Brawl Pass Plus",price:1355,img:"img/BrawlPassPlus.png"},
- {id:9,name:"Brawl Pass Upgrade",price:500,img:"img/PBUPGREAD.png"},
- {id:10,name:"Pro Pass",price:2650,img:"img/ProPass.png"},
- {id:11,name:"Пин Матчерино Мэнди",price:400,img:"img/MatcherinoMandy.png"},
- {id:12,name:"Пин Матчерино Спайк",price:650,img:"img/MatcherinoSpike.png"},
- {id:13,name:"Особая акция",price:400,img:"img/SelaPass.jpg"},
+ {id:1,name:"Гемы",price:255,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\30.png"},
+ {id:2,name:"Гемы",price:600,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\80.png"},
+ {id:3,name:"Гемы",price:1100,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\170.png"},
+ {id:4,name:"Гемы",price:2100,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\360.png"},
+ {id:5,name:"Гемы",price:5150,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\950.png"},
+ {id:6,name:"Гемы",price:10350,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\2000.png"},
+ {id:7,name:"Brawl Pass",price:950,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\BrawlPass.png"},
+ {id:8,name:"Brawl Pass Plus",price:1355,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\BrawlPassPlus.png"},
+ {id:9,name:"Brawl Pass UPGREAD",price:500,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\PBUPGREAD.png"},
+ {id:10,name:"PRO PASS",price:2650,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\ProPass.png"},
+ {id:11,name:"Пин Матчерино с Менди",price:400,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\MatcherinoMandy.png"},
+ {id:12,name:"Пин Матчерино со Спайком",price:650,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\MatcherinoSpike.png"},
+ {id:13,name:"Особая АКЦИЯ ",price:400,img:"C:\Users\Матвей\Downloads\cosmo-mini-app-final-1768475707\img\SelaPass.jpg"},
 ];
-
 
 const STATUSES = [
  "Ожидает оплату",
